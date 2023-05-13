c5a18dafad590b4a1cf6144710433ea8b89f2f07
emotionl <emotionl@yeah.net>
2023-05-14 02:29:15 +0800

    :wrench: update bundle config

--- rollup.config.js
+++ rollup.config.js
@@ -11,13 +11,13 @@ export default [
     input: 'src/index.ts',
     output: [
       {
-        file: 'dist/index.js',
-        format: 'cjs',
+        file: 'dist/index.esm.js',
+        format: 'esm',
         sourcemap: true
       },
       {
-        file: 'dist/index.esm.js',
-        format: 'esm',
+        file: 'dist/index.cjs.js',
+        format: 'cjs',
         sourcemap: true
       },
       {
